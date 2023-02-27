e8c8d5e1b074ecabebea5f78646cab8211e7c413
Masha <suvorova-2007@mail.ru>
2023-02-27 16:30:43 +0300

    Find length of new  array

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,4 +1,20 @@
 ﻿/* Из введенного массива строк необходимо сформировать новый массив из строк,
 длина которых меньше либо равна 3 символа */
 
+int SorteredArrayLength(string[] array)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string element = array[i];
+        if (element.Length <= 3)
+        {
+            n+=1;
+        }
+    }
+    return n;
+}
 
+Console.WriteLine("Введите строки массива через пробел");
+string charInLine = Console.ReadLine();
+string[] arrayOfLines = charInLine.Split(" ");
